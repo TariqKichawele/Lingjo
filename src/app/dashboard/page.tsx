b2b579392594cf9e48aa792b87608a213abc993e
TariqKichawele <tariqkichawele66@gmail.com>
2024-12-02 16:45:01 -0500

    Implemented auth callback to save user in db

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import MaxWidthWrapper from '@/components/common/MaxWidthWrapper'
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <MaxWidthWrapper>
+      <div>
+        Dashboard
+      </div>
+    </MaxWidthWrapper>
+  )
+}
+
+export default Dashboard
